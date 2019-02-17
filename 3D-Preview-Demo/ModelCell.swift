d28c6e8fcbebc5370d3c4d422bcf4117de68a4de
fangqk1991 <me@fangqk.com>
2019-02-17 16:46:52 +0800

    AR QuickLook

---
+++ 3D-Preview-Demo/ModelCell.swift
@@ -0,0 +1,14 @@
+//
+//  ModelCell.swift
+//  3D-Preview-Demo
+//
+//  Created by fang on 2019/2/17.
+//  Copyright © 2019 fangcha. All rights reserved.
+//
+
+import UIKit
+
+class ModelCell: UICollectionViewCell {
+    @IBOutlet weak var modelThumbnail: UIImageView!
+    @IBOutlet weak var modelTitle: UILabel!
+}
---
+++ 3D-Preview-Demo/ModelListViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ModelListViewController.swift
+//  3D-Preview-Demo
+//
+//  Created by fang on 2019/2/17.
+//  Copyright © 2019 fangcha. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import QuickLook
+
+class ModelListViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, QLPreviewControllerDelegate, QLPreviewControllerDataSource {
+    
+    func numberOfPreviewItems(in controller: QLPreviewController) -> Int {
+        return 1
+    }
+    
+    func previewController(_ controller: QLPreviewController, previewItemAt index: Int) -> QLPreviewItem {
+        let url = Bundle.main.url(forResource: models[thumbnailIndex], withExtension: "usdz")!
+        return url as QLPreviewItem
+    }
+    
+    @IBOutlet var collectionView: UICollectionView!
+    let models = ["wheelbarrow", "wateringcan", "teapot", "gramophone", "cupandsaucer", "redchair", "tulip", "plantpot"]
+    
+    var thumbnails = [UIImage]()
+    var thumbnailIndex = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        for model in models {
+            if let thumbnail = UIImage(named: model) {
+                thumbnails.append(thumbnail)
+            }
+        }
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.reloadData()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ModelCell", for: indexPath) as? ModelCell
+        
+        if let cell = cell {
+            cell.modelThumbnail.image = thumbnails[indexPath.item]
+            let title = models[indexPath.item]
+            cell.modelTitle.text = title.capitalized
+        }
+        
+        return cell!
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        thumbnailIndex = indexPath.item
+        
+        let previewController = QLPreviewController()
+        previewController.dataSource = self
+        previewController.delegate = self
+        present(previewController, animated: true)
+    }
+    
+}
+
